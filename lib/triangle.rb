33e07fb1b69f5121da99b1638315657cf514b6ab
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-26 21:10:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,13 @@ class Triangle
   def sort
     @array.sort
   end
+  def isc(sort)
+    if sort[1] == sort[0]|| sort[2]==sort[1]
+      return "true"
+    else
+      return "false"
+  end
+
   class TriangleError < StandardError
     # triangle error code
   end
